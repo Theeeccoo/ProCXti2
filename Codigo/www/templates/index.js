777f4d645b209f1fc6040b27859a39ed9a83c713
Thiago de Campos <thiagodecampos1@gmail.com>
2021-05-14 20:33:32 -0300

    ADD arquivos iniciais

---
+++ Codigo/www/templates/index.js
@@ -0,0 +1,8 @@
+// MODAL POPUP - Início
+function openLoginForm(){
+    document.body.classList.add("showLoginForm")
+}
+function closeLoginForm(){
+    document.body.classList.remove("showLoginForm")
+}
+// MODAL POPUP - Fim
