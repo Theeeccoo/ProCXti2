a593b2242ab869f50fbd829388461fa585293417
Thiago de Campos <thiagodecampos1@gmail.com>
2021-05-17 16:35:28 -0300

    UPDATE arquivos principais v.2

--- Codigo/www/templates/index.js
+++ Codigo/www/templates/index.js
@@ -3,6 +3,10 @@ function openLoginForm(){
     document.body.classList.add("showLoginForm")
 }
 function closeLoginForm(){
-    document.body.classList.remove("showLoginForm")
+    document.body.classList.remove("showLoginForm") 
+    document.body.classList.remove("showLoginForm2") 
+}
+function openLoginForm2(){
+    document.body.classList.add("showLoginForm2")
 }
 // MODAL POPUP - Fim
