2828af56179a935c0e6b639941d2a2052c324969
Thiago de Campos <thiagodecampos1@gmail.com>
2021-05-26 19:46:18 -0300

    ADD ordenação de atividades e alteração nas configurações do perfil

--- Codigo/www/scripts/perfil.js
+++ Codigo/www/scripts/perfil.js
@@ -27,53 +27,52 @@ function ValidarSession() {
 
 ValidarSession()
 
-function verificarData()
-{
+function verificarData() {
     Users = JSON.parse(window.localStorage.getItem("users"));
     UsuarioAtual = Users[SessionID]
 
 
     var data = new Date();
     const dia = data.getDate()
-        const mes = data.getMonth()
-        const ano = data.getFullYear()
-        dataAtual = `${dia}/${mes}/${ano}`
+    const mes = data.getMonth()
+    const ano = data.getFullYear()
+    dataAtual = `${dia}/${mes}/${ano}`
 
-        if(UsuarioAtual.lastSeen != 'dataAtual'){
-            ativs = JSON.parse(window.localStorage.getItem(`atividades${SessionID}`));
-            
-            if(ativs != null){
-             
-            
-             let  ArrayAtividadesFeitas = []
-               
-             //Esse map povoa um novo array com as informações das atividades feitas na ultima data de 
-             //atividade diferente da do dia atual e reseta o array de atividades do local storage sendo a
-             //propriedade "checked" delas sendo false
-               const newAtivs = ativs.map(ativ=>{
-                
+    if (UsuarioAtual.lastSeen != 'dataAtual') {
+        ativs = JSON.parse(window.localStorage.getItem(`atividades${SessionID}`));
 
-               ArrayAtividadesFeitas = [...ArrayAtividadesFeitas,{nome:ativ.nome,realizada:ativ.checked}]                  
- 
-                  return {...ativ,checked:false}
-               }
-              
-               )
-               localStorage.setItem(`atividades${SessionID}`,JSON.stringify(newAtivs));
+        if (ativs != null) {
 
-               atividadesFeitas = JSON.parse(window.localStorage.getItem(`atividadesRealizadas${SessionID}`));
 
-               objAtividadesFeitas = {data:UsuarioAtual.lastSeen, ArrayAtividadesFeitas}
+            let ArrayAtividadesFeitas = []
 
-               if(atividadesFeitas == null){
-                localStorage.setItem(`atividadesRealizadas${SessionID}`,JSON.stringify([objAtividadesFeitas]));
-               }else{
-                   atividadesFeitas.push(objAtividadesFeitas)
-                   localStorage.setItem(`atividadesRealizadas${SessionID}`,JSON.stringify(atividadesFeitas));
-               }
-               
+            //Esse map povoa um novo array com as informações das atividades feitas na ultima data de 
+            //atividade diferente da do dia atual e reseta o array de atividades do local storage sendo a
+            //propriedade "checked" delas sendo false
+            const newAtivs = ativs.map(ativ => {
+
+
+                ArrayAtividadesFeitas = [...ArrayAtividadesFeitas, { nome: ativ.nome, realizada: ativ.checked }]
+
+                return { ...ativ, checked: false }
             }
+
+            )
+            localStorage.setItem(`atividades${SessionID}`, JSON.stringify(newAtivs));
+
+            atividadesFeitas = JSON.parse(window.localStorage.getItem(`atividadesRealizadas${SessionID}`));
+
+            objAtividadesFeitas = { data: UsuarioAtual.lastSeen, ArrayAtividadesFeitas }
+
+            if (atividadesFeitas == null) {
+                localStorage.setItem(`atividadesRealizadas${SessionID}`, JSON.stringify([objAtividadesFeitas]));
+            } else {
+                atividadesFeitas.push(objAtividadesFeitas)
+                localStorage.setItem(`atividadesRealizadas${SessionID}`, JSON.stringify(atividadesFeitas));
+            }
+
         }
+    }
 }
 
 verificarData()
@@ -89,25 +88,51 @@ function ListarUsuario() {
 }
 ListarUsuario()
 
-function ListarAtividades(){
+function ListarAtividades() {
 
     const Atividades = JSON.parse(window.localStorage.getItem(`atividades${SessionID}`))
 
     const titulo = document.getElementById('titulo_atividades')
     const block_ativ = document.getElementById('Atividades')
 
-    
-    
-
-    if(Atividades == null || Atividades == []){
+    if (Atividades == null || Atividades == []) {
         titulo.innerHTML = `Você ainda não tem atividades...`
-        
-    }else{
-        titulo.innerHTML =''
-        let obj =''
- Atividades.map(ativ=>{
-     
-    obj += `
+
+    } else {
+        // ~ Odenação das atividades utilizando um "Bubble Sorter" ~ \\
+        if(Atividades.length > 1){ // ~ Só será necessário fazer a ordenação caso existam mais de 1 atividade
+            for(let i = 0; i < Atividades.length; i++){
+                // Atribuição via desestruturação                 ~ Separando o objeto horário em dois apartir do ":"
+                let [arrayHorarioPosterior, arrayMinutoPosterior] = Atividades[i].horario.split(":"); 
+
+                for(let j = 0; j < Atividades.length; j++){
+                    // Atribuição via desestruturação               ~ Separando o objeto horário em dois apartir do ":"
+                    let [arrayHorarioAnterior, arrayMinutoAnterior] = Atividades[j].horario.split(":");
+
+                    // ~ Verificações necessárias para fazer a ordenação de acordo com o horário ~ \\
+                    // Caso a HORA do primeiro seja maior do que a HORA do segundo, "troque o índice" dos dois 
+                    if (arrayHorarioAnterior > arrayHorarioPosterior){ 
+                        aux = Atividades[j]
+                        Atividades[j] = Atividades[i]
+                        Atividades[i] = aux;
+                    }
+                    
+                    // Caso a HORA do primeiro seja igual a HORA do segundo, verifique os MINUTOS
+                    if(arrayHorarioAnterior == arrayHorarioPosterior){
+                        if(arrayMinutoAnterior[j] > arrayMinutoPosterior[i]){
+                            aux = Atividades[j]
+                            Atividades[j] = Atividades[i]
+                            Atividades[i] = aux;
+                        }
+                    }
+                }// Fim do FOR interno (J == Atividades.length)
+            }// Fim do FOR externo ( I == Atividades.length)
+        }
+        titulo.innerHTML = ''
+        let obj = ''
+        Atividades.map(ativ => {
+
+            obj += `
 <div class='col-12 col-md-4 box-atividades' >
 <h5>${ativ.nome}</h5>
 
@@ -122,31 +147,31 @@ function ListarAtividades(){
         <p class='horario'>${ativ.horario}</p>
         <input type='checkbox' onchange="isChecked(${ativ.id})" name='' id='' class='check' />
         </div> `  }
-    
+
 
         )
-        
+
         block_ativ.innerHTML = obj
 
 
-        
+
     }
 }
 ListarAtividades()
 
 
-function sucesso(){
+function sucesso() {
     Swal.fire({
-  
+
         icon: 'success',
         title: 'Atividade registrada com sucesso',
         showConfirmButton: false,
         timer: 1500,
 
-      })
+    })
 
-    document.body.classList.remove("showLoginForm") 
-    document.body.classList.remove("showLoginForm2") 
+    document.body.classList.remove("showLoginForm")
+    document.body.classList.remove("showLoginForm2")
     ListarAtividades()
 }
 
@@ -156,52 +181,52 @@ function criarAtividades() {
     const nome = document.getElementById("txt_nome_ativ").value
     const horario = document.getElementById("txt_horario_ativ").value
     const descricao = document.getElementById("txt_descricao_ativ").value
-   
+
 
     if (nome != '' || horario != '' || descricao != '') {
-        
-      
+
+
         const Atividades = JSON.parse(window.localStorage.getItem(`atividades${SessionID}`))
 
-        if(Atividades == null || Atividades == []){
+        if (Atividades == null || Atividades == []) {
             const NovaAtividade = {
-                id:0,
-                nome:nome,
-                horario:horario,
-                descricao:descricao,
-                checked:false
-    
+                id: 0,
+                nome: nome,
+                horario: horario,
+                descricao: descricao,
+                checked: false
+
             }
             const AtividadesArray = [NovaAtividade]
-            localStorage.setItem(`atividades${SessionID}`,JSON.stringify(AtividadesArray));
+            localStorage.setItem(`atividades${SessionID}`, JSON.stringify(AtividadesArray));
 
-           sucesso()
+            sucesso()
         }
-        else{
+        else {
             const NovaAtividade = {
-                id:Atividades.length,
-                nome:nome,
-                horario:horario,
-                descricao:descricao,
-                checked:false
-    
+                id: Atividades.length,
+                nome: nome,
+                horario: horario,
+                descricao: descricao,
+                checked: false
+
             }
 
             Atividades.push(NovaAtividade)
-            localStorage.setItem(`atividades${SessionID}`,JSON.stringify(Atividades));
+            localStorage.setItem(`atividades${SessionID}`, JSON.stringify(Atividades));
 
-          sucesso();
+            sucesso();
         }
 
-    }else{
+    } else {
         Swal.fire({
-  
+
             icon: 'warning',
             title: 'Preecha todos os campos',
             showConfirmButton: false,
             timer: 1500,
-        
-          })
+
+        })
     }
 }
 
